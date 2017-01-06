ee620e813ec649bee02d1606d021df2f9299efd4
Susanne van den Elsen <sanne-acc@posteo.net>
2017-01-06 14:42:00 +0100

    [refactor] added function download_and_untar and included compiler-rt project

--- download_and_install_llvm.sh
+++ download_and_install_llvm.sh
@@ -7,50 +7,54 @@ here=`pwd .`
 
 llvm_base=${prefix}/llvm-${version}
 llvm_src=${llvm_base}/src
+llvm_projects_src=${llvm_src}/projects
 llvm_build=${llvm_base}/build
+
+# source locations of subprojects
 clang_src=${llvm_src}/tools/clang
-libcxx_src=${llvm_src}/projects/libcxx
-libcxxabi_src=${llvm_src}/projects/libcxxabi
+compiler_rt_src=${llvm_projects_src}/compiler-rt
+libcxx_src=${llvm_projects_src}/libcxx
+libcxxabi_src=${llvm_projects_src}/libcxxabi
 
 llvm_download_page=http://releases.llvm.org/${version}
 
-echo ===== Building LLVM-${version} in base dir ${llvm_base} for target ${target}
+echo ===== Building LLVM-${version} in base dir ${llvm_base}
 
-echo === Checking files
+test -d ${llvm_base} || mkdir -p ${llvm_base}
+test -d ${llvm_build} || mkdir ${llvm_build}
+
+echo === Checking or downloading releases
 
 downloads=${prefix}/downloads
 test -d ${downloads} || (echo dir ${downloads} does not exist && mkdir downloads)
 
-# llvm
-llvm_tar=llvm-${version}.src.tar.xz
-test -f ${downloads}/${llvm_tar} || wget ${llvm_download_page}/${llvm_tar} -P ${downloads}
+#
+# download_and_untar(tar_name, folder, src_destinaton)
+#
+download_and_untar() {
+   tar_name=`echo $1`
+   folder=`echo $2`
+   src_destination=`echo $3`
+   echo  $'\n'Downloading and Untarring ${tar_name} to ${src_destination}
+   untarred=${tar_name}-${version}.src
+   tar_file=${untarred}.tar.xz
+   test -f ${downloads}/${tar_file} || wget ${llvm_download_page}/${tar_file} -P ${downloads}
+   test -d ${src_destination} || (tar xf ${downloads}/${tar_file} -C ${folder} && mv ${folder}/${untarred} ${src_destination})
+}
+#
+#
+#
 
-# clang
-clang_tar=cfe-${version}.src.tar.xz
-test -f ${downloads}/${clang_tar} || wget ${llvm_download_page}/${clang_tar} -P ${downloads}
-
-# libc++
-libcxx_tar=libcxx-${version}.src.tar.xz
-test -f ${downloads}/${libcxx_tar} || wget ${llvm_download_page}/${libcxx_tar} -P ${downloads}
-
-# libc++abi
-libcxxabi_tar=libcxxabi-${version}.src.tar.xz
-test -f ${downloads}/${libcxxabi_tar} || wget ${llvm_download_page}/${libcxxabi_tar} -P ${downloads}
-
-echo === Setup project dirs
-
-test -d ${llvm_base} || (echo Creating dir ${llvm_base} && mkdir -p ${llvm_base})
-test -d ${llvm_src} || (echo Untarring llvm sources to ${llvm_src} && tar xf ${downloads}/${llvm_tar} -C ${llvm_base} && mv ${llvm_base}/llvm-${version}.src ${llvm_src})
-test -d ${clang_src} || (echo Untarring clang sources to ${clang_src} && tar xf ${downloads}/${clang_tar} -C ${llvm_src}/tools && mv ${llvm_src}/tools/cfe-${version}.src ${clang_src})
-test -d ${libcxx_src} || (echo Untarring libcxx sources to ${libcxx_src} && tar xf ${downloads}/${libcxx_tar} -C ${llvm_src}/projects && mv ${llvm_src}/projects/libcxx-${version}.src ${libcxx_src})
-test -d ${libcxxabi_src} || (echo Untarring libcxxabi sources to ${libcxxabi_src} && tar xf ${downloads}/${libcxxabi_tar} -C ${llvm_src}/projects && mv ${llvm_src}/projects/libcxxabi-${version}.src ${libcxxabi_src})
-test -d ${llvm_build} || (echo Creating dir ${llvm_build} && mkdir ${llvm_build})
+download_and_untar llvm ${llvm_base} ${llvm_src}
+download_and_untar cfe ${llvm_src}/tools ${clang_src}
+download_and_untar compiler-rt ${llvm_projects_src} ${compiler_rt_src}
+download_and_untar libcxx ${llvm_projects_src} ${libcxx_src}
+download_and_untar libcxxabi ${llvm_projects_src} ${libcxxabi_src}
 
 echo === Making
 
 cd ${llvm_build}
 cmake ../src -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_EH=ON -DLLVM_TARGETS_TO_BUILD=all
 cmake --build .
-cmake --build . --target install
+#cmake --build . --target install
 cd ${here}
-
