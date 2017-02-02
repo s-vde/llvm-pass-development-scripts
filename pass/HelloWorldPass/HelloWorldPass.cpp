ff145213742835050edcd9702b63c1ec3b0be00f
sanne <sanne-acc@posteo.net>
2017-02-02 10:31:37 +0100

    [new file] added HelloWorldPass with cmake files for building an LLVM pass out-of-source

---
+++ pass/HelloWorldPass/HelloWorldPass.cpp
@@ -0,0 +1,30 @@
+
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace
+{
+    struct HelloWorldPass : public llvm::FunctionPass
+    {
+        static char ID;
+        
+        HelloWorldPass() : llvm::FunctionPass(ID)
+        {
+        }
+
+        bool runOnFunction(llvm::Function &F) override
+        {
+            ++mFunctionCounter;
+            llvm::errs() << "Hello: ";
+            llvm::errs().write_escaped(F.getName()) << std::endl;
+            return false;
+        }
+        
+        unsigned int mFunctionCounter = 0;
+        
+    }; // end struct HelloWorldPass
+} // end namespace
+
+char HelloWorldPass::ID = 0;
+static llvm::RegisterPass<HelloWorldPass> X("hello-world", "Hello World Pass");
